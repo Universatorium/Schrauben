f5789fd54723d3cd28a3d99f2393c3e0bd82d958
Thomas Kirsch <thomas.kirsch@yahoo.de>
2023-07-04 15:46:27 +0200

    in arbeit

--- controllers/schraubenController.js
+++ controllers/schraubenController.js
@@ -82,44 +82,5 @@ exports.getIndexPage = asyncHandler(async (req, res, next) => {
 //   res.render("index", { schrauben, charts });
 // });
 
-// exports.gettopsales = asyncHandler(async (req, res, next) => {
-//   const topSchrauben = await schraube.aggregate([
-//     { $sort: { VerkaufteMenge: -1 } },
-//     { $group: { _id: "$Schraube", VerkaufteMenge: { $first: "$VerkaufteMenge" } } },
-//     { $sort: { VerkaufteMenge: -1 } },
-//     { $limit: 3 },
-//     { $project: { _id: 0, Schraube: "$_id", VerkaufteMenge: 1 } }
-//   ]);
-//   res.render("index", { schrauben: topSchrauben, charts });
-// });
-
-// exports.getTopHersteller = asyncHandler(async (req, res, next) => {
-//   const topHersteller = await schraube.aggregate([
-//     { $group: { _id: "$Hersteller", VerkaufteMenge: { $sum: "$VerkaufteMenge" } } },
-//     { $sort: { VerkaufteMenge: -1 } },
-//     { $limit: 3 },
-//     { $project: { _id: 0, Hersteller: "$_id", VerkaufteMenge: 1 } }
-//   ]);
-//   res.render("index", { schrauben : topHersteller, charts  });
-// });
-
-
-// exports.getBestSellingDay = asyncHandler(async (req, res, next) => {
-//   const bestday = await schraube.aggregate([
-//     {
-//       $group: {
-//         _id: {
-//           year: { $year: { $toDate: "$Datum" } }, // Konvertiert den String in ein Datum
-//           month: { $month: { $toDate: "$Datum" } },
-//           day: { $dayOfMonth: { $toDate: "$Datum" } }
-//         },
-//         totalSales: { $sum: "$VerkaufteMenge" }
-//       }
-//     },
-//     { $sort: { totalSales: -1 } },
-//     { $limit: 1 }
-//   ]);
-//   res.render("index", { schrauben : bestday, charts });
-// });
 
 
