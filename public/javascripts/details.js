f96c85c103439fbbd3ed36ff26535b6e7fcb0900
Universatorium <matthias.bandholtz@google.com>
2023-07-07 11:02:12 +0200

    Spielereien

--- public/javascripts/details.js
+++ public/javascripts/details.js
@@ -16,9 +16,23 @@ function drawdetails(data,hersteller) {
       }]
     },
     options: {
+      animations: {
+        tension: {
+          duration: 1000,
+          easing: 'linear',
+          from: 1,
+          to: 0,
+          loop: false
+        }
+      },
+      layout: {
+        padding: {
+            left: 50
+        }
+    },
       scales: {
         r: {
-          beginAtZero: false
+          beginAtZero: true
         }
       }
     }
